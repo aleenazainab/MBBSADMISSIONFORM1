8d2c762ece9e118098edcc2212e073347b34ff90
aleenazainab <79971986+aleenazainab@users.noreply.github.com>
2024-10-24 10:42:31 -0700

    Update Form.js

--- src/Components/Landing/Form/Form.js
+++ src/Components/Landing/Form/Form.js
@@ -486,7 +486,7 @@ style={{ textTransform: 'uppercase' }}
 
                 
                 <div className='form-group'>
-    <label>Student's Date of Birth:</label>
+    <label>Student's Date of Birth(yyyy/mm/dd):</label>
     <input
         type="date"
         id="dob"
